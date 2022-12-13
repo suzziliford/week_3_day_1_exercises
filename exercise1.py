0da5e9990a475fbe1abf0ddb198bf629f150f444
Matty Witney <mattwitney@gmail.com>
2022-12-13 08:23:21 -0600

    exercise 1 added

---
+++ exercise1.py
@@ -0,0 +1,61 @@
+def fill_cart():
+
+    class Shop:
+
+        def __init__(self, customer_name):
+            self.first_name = first.lower()
+            self.last_name = last.lower()
+            self.cart = []
+
+
+        def get_full_name(self):
+            return self.first_name + ' ' + self.last_name
+            # customer_name = input(first_name + " " + last_name)
+            # return self.customer_name
+
+customer = Shop('a', 'b', 'c')
+
+
+        def add_to_cart(self):
+            return self.cart
+                for item in self.cart:
+                self.cart.append(a)
+
+
+        def show_cart(self):
+            pass
+            
+        def run():
+            my_cart = Shop('Brian')
+            while True:
+                customer.first_name = input ("Hi, what is your first name?: ").lower()
+                customer.last_name = input ("What is your last name?: ")
+                cart = input("What would you like to add?: Or would you like to checkout by typing 'quit'?")
+                if ask == 'add': 
+                    a = input("What item would you like to add?")
+                    my_cart.add_to_cart()
+                elif ask == 'quit':
+                    break
+            
+        
+
+        # def remove_from_cart(self):
+            #         elif opening_choice == "remove":
+            # to_remove = input("What item would you like to remove?: ")
+            # if to_remove in cart:
+            #     new_quantity = int(input("How many would you like in total?"))     
+            #     old_name, old_quantity, old_price, old_total = cart[to_remove]
+            #     new_total = new_quantity * old_price
+            #     new_entry = (old_name, new_quantity, old_price, new_total)
+            #     cart[to_remove] = new_entry
+
+
+    first_name = input("Hi, what is your first name?: ").lower()
+    last_name = input("What is your last name?: ").lower()
+    customer_name = (first_name + " " + last_name)
+    print(customer_name)
+
+        
+
+fill_cart()                
+    # run()
